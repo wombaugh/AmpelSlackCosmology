c0d490a5f19de39e9a100fbfafb789259837b79e
MatteoGiomi <matteo.giomi@desy.de>
2018-09-28 10:05:31 +0200

    move user details out of notebooks

---
+++ user_config.py
@@ -0,0 +1,5 @@
+# stores user specific configuration, removing them from the notebooks
+
+username = 'mgiomi'
+logpath = '/home/matteo/work/myGits/AmpelSlackCosmology/logs'
+slackfilepath = '/home/matteo/Downloads/'
